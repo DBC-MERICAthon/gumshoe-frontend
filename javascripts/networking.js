585aa98c253b3f2a0199de51d51d6eccfad6366f
XanderPSON <xwpeterson@gmail.com>
2016-02-14 06:30:27 -0800

    Add networking page css

--- javascripts/networking.js
+++ javascripts/networking.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   	// e.preventDefault();
 		// setTimeout(loadDigitalPrint, 3000);
 	// });
-		setTimeout(loadTerms, 20000);
+		setTimeout(loadTerms, 200);
 		//   var terms = ['japan', 'jet', 'programming', 'videogames'];
   // for (i = 0; i < terms.length; i++) {
   // 	console.log(terms[i]);
@@ -15,12 +15,42 @@ function loadDigitalPrint(){
 }
 
 function loadTerms(){
-  var terms = dataObj.xanTerms;
+  // console.log(dataObj.xanTerms[0])
+  var terms = ['japan', 'jet', 'videogames', 'coding', 'ucla', 'california', 'beard'];
   for (i = 0; i < terms.length; i++) {
   	terms[i] = terms[i].charAt(0).toUpperCase() + terms[i].slice(1);
   }
   for (i = 0; i < terms.length; i++) {
-  	$('#my-terms').append('<li>'+terms[i]+'</li>')
-  	$('#other-terms').append('<li>'+terms[terms.length - i - 1]+'</li>')
+  	$('#left-terms').append('<li>'+terms[i]+'</li>')
+  	$('#right-terms').append('<li>'+terms[terms.length - i - 1]+'</li>')
   }
 }
+
+
+
+// $(document).ready(function(){
+//   // $('body').on('click', '.left-profile', function(e){
+//   	// e.preventDefault();
+// 		// setTimeout(loadDigitalPrint, 3000);
+// 	// });
+// 		setTimeout(loadTerms, 20000);
+// 		//   var terms = ['japan', 'jet', 'programming', 'videogames'];
+//   // for (i = 0; i < terms.length; i++) {
+//   // 	console.log(terms[i]);
+//   // }
+// });
+
+// function loadDigitalPrint(){
+//   appendImg('my-fingerprint', dataObj.xander)
+// }
+
+// function loadTerms(){
+//   var terms = dataObj.xanTerms;
+//   for (i = 0; i < terms.length; i++) {
+//   	terms[i] = terms[i].charAt(0).toUpperCase() + terms[i].slice(1);
+//   }
+//   for (i = 0; i < terms.length; i++) {
+//   	$('#left-terms').append('<li>'+terms[i]+'</li>')
+//   	$('#right-terms').append('<li>'+terms[terms.length - i - 1]+'</li>')
+//   }
+// }
