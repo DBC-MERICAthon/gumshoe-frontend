965ebb3bf1c28847a5962a302c033a998f9993e2
XanderPSON <xwpeterson@gmail.com>
2016-02-14 08:42:13 -0800

    Add spinning logo turned green and image swap function

--- javascripts/networking.js
+++ javascripts/networking.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	setTimeout(loadTerms, 200);
+  setTimeout(loadTerms, 200);
   setTimeout(loadDigitalPrint, 3000);
 
   $('body').on('click', '#cortical-button-search', function(e){
     e.preventDefault();
     rotateAnimation("cortical-button-search",1,0);
   });
-  
+
 });
 
 function loadDigitalPrint(){
@@ -18,11 +18,11 @@ function loadTerms(){
   // var terms = dataObj.xanTerms;
   var terms = ['japan', 'jet', 'videogames', 'coding', 'ucla', 'california', 'beard'];
   for (i = 0; i < terms.length; i++) {
-  	terms[i] = terms[i].charAt(0).toUpperCase() + terms[i].slice(1);
+    terms[i] = terms[i].charAt(0).toUpperCase() + terms[i].slice(1);
   }
   for (i = 0; i < terms.length; i++) {
-  	$('#left-terms').append('<li>'+terms[i]+'</li>')
-  	$('#right-terms').append('<li>'+terms[terms.length - i - 1]+'</li>')
+    $('#left-terms').append('<li>'+terms[i]+'</li>')
+    $('#right-terms').append('<li>'+terms[terms.length - i - 1]+'</li>')
   }
 }
 
@@ -30,8 +30,16 @@ function rotateAnimation(el,speed,degrees){
   var elem = document.getElementById(el);
   elem.style.WebkitTransform = "rotate("+degrees+"deg)";
   degrees++;
-  if(degrees > 359){
-    degrees = 1;
+  if(degrees > 360){
+    $('#cortical-button-search').hide();
+    $('#cortical-button-search-green').show();
+    swapMatchData();
+    return;
   }
-  // setTimeout('rotateAnimation(\''+el+'\','+speed+','+degrees+')',speed);
+  setTimeout('rotateAnimation(\''+el+'\','+speed+','+degrees+')',speed);
+}
+
+function swapMatchData() {
+  console.log('hit');
+  $('#right-profile-image').attr("src","../images/jesse-headshot.jpg");
 }
