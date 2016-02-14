5296f865a52b607b0c5e33fe9126912d57e33c4f
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2016-02-14 08:10:19 -0800

    clean up JS

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -6,7 +6,7 @@
 //     });
 // }
 
-	function closeResults(){
+function closeResults(){
 
 		$('#results-view').removeClass('open');
 		$('#results-view').animate({'opacity':0},300);
@@ -19,23 +19,14 @@
 		},300)
 	}
 
-	$('.search-again').click(function(){
-		closeResults();
-	})
+	// $('.search-again').click(function(){
+	// 	closeResults();
+	// })
 
-
-// 	function generateRandom() {
-//   return Math.floor(Math.random() * 300);
-// };
-
-});
 function openResults(){
 	$('#results-view').show()
 	$('.wrapper').hide()
-	$('#results-view').addClass('open');    //Makes prject div appear
-	$('.container').animate({'opacity':0},300);//Makes projects disappear
+	// $('#results-view').addClass('open');    //Makes prject div appear
+	// $('.container').animate({'opacity':0},300);//Makes projects disappear
 
-	setTimeout(function(){
-		$('#results-view').slideDown();
-		$('.container').slideUp();
-},300);
+}
